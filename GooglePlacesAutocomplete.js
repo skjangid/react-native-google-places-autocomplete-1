f6e5c5b0fa83e4ea79e087cc7295a6e4397571fa
Sanjay Jangid <jangid005@yahoo.com>
2025-09-24 19:39:38 +0530

    Update GooglePlacesAutocomplete.js
    Remove ref

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -723,7 +723,7 @@ export default class GooglePlacesAutocomplete extends Component {
           >
             {this._renderLeftButton()}
             <TextInputComp
-              ref="textInput"
+              // ref="textInput"
               editable={this.props.editable}
               returnKeyType={this.props.returnKeyType}
               keyboardAppearance={this.props.keyboardAppearance}
